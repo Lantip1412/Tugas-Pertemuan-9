a51000df8bdf852eaa4e3f4b8c28511837c28628
Lantip1412 <lantip.1412@gmail.com>
2024-11-19 02:34:46 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<?php
+
+$nama   = "Lantip Nur Ichsan";
+$Kelas  = "TI.23.CA.1";
+$Nim    = "312310519";
+
+?>
+
+<body>
+  <h1>
+    Saya Adalah <?php echo $nama ?>
+  </h1>
+  <p>
+    Dari Kelas = <?php echo $Kelas ?>
+  </p>
+  <p>
+    Dengan Nim = <?php echo $Nim ?>
+  </p>
+</body>
+
+</html>
